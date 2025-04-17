7916b58406352191b90dfc5a42ec6f606d2bb9f9
Charlatan <legitimatecharlatan@gmail.com>
2025-04-17 17:57:29 +0100

    fix base href

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,10 @@ const config = {
     kit: {
         adapter: adapter({
             fallback: "index.html"
-        })
+        }),
+        paths: {
+            base: process.env.NODE_ENV === 'production' ? '/web-playground' : '',
+        }
     }
 };
 
